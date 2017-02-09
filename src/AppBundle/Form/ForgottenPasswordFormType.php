5396cbb4070dac36d4aa8951d51a57a24ae4e4ed
Lucien Akchoté <l.akchote@gmail.com>
2017-02-09 12:12:05 +0100

    Fix merge

--- src/AppBundle/Form/ForgottenPasswordFormType.php
+++ src/AppBundle/Form/ForgottenPasswordFormType.php
@@ -2,24 +2,10 @@
 
 namespace AppBundle\Form;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-use AppBundle\Entity\User;
->>>>>>> Closes #9, Closes #10
-=======
->>>>>>> origin/user
 use AppBundle\Validator\Constraints\UsernameExists;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-use Symfony\Component\OptionsResolver\OptionsResolver;
->>>>>>> Closes #9, Closes #10
-=======
->>>>>>> origin/user
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ForgottenPasswordFormType extends AbstractType
@@ -32,15 +18,4 @@ class ForgottenPasswordFormType extends AbstractType
                 'constraints' => array(new UsernameExists(), new NotBlank())
             ]);
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-    public function configureOptions(OptionsResolver $resolver)
-    {
-
-    }
->>>>>>> Closes #9, Closes #10
-=======
->>>>>>> origin/user
 }
