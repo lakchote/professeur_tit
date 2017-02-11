ef3ed8a683068fe21ed94e6d2cb3bd2af3ec6af5
BenY <beny.lagache@gmail.com>
2017-02-11 22:33:41 +0100

    Changed the autocomplete to query "on request" the database

--- src/AppBundle/Controller/ObservationController.php
+++ src/AppBundle/Controller/ObservationController.php
@@ -16,10 +16,7 @@ class ObservationController extends Controller
      */
     public function indexAction()
     {
-        $listeTaxons = $this->get('app.createliste')->createList();
-        return $this->render('default/obs.html.twig', [
-            'taxonListe' => $listeTaxons,
-        ]);
+        return $this->render('default/obs.html.twig');
     }
 
     /**
@@ -28,14 +25,20 @@ class ObservationController extends Controller
     public function getModalDesktopAction(Request $request)
     {
         if($request->isXmlHttpRequest()) {
-
             $modal= $this->createForm(ObsFormType::class);
             return $this->render('modal/modal_add_obs_desktop.html.twig', [
                 'form' => $modal->createView(),
-
             ]);
         }
         return new Response('Type de requête invalide');
     }
 
+    /**
+     * @Route("/getListing", name="getListing")
+     */
+    public function getLisitngAction(Request $request)
+    {
+            $listeTaxons = $this->get('app.createliste')->createList($_GET['term']);
+            return  $this->json($listeTaxons);
+    }
 }
