ea0634a79b5bb973e4af86a67990a8c11bf47fff
BenY <beny.lagache@gmail.com>
2017-03-30 08:27:11 +0200

    -> Push due to computer change... Added Knp Pagination Bundle Worked on observation views

--- src/AppBundle/Repository/ObservationRepository.php
+++ src/AppBundle/Repository/ObservationRepository.php
@@ -22,10 +22,20 @@ class ObservationRepository extends \Doctrine\ORM\EntityRepository
             ->leftJoin('obs.user', 'user')
             ->andWhere('user.id = :id')
             ->setParameter('id', $id)
+            ->getQuery();
+    }
+    /*
+    public function getUserObservationsFlow($id)
+    {
+        return $this->createQueryBuilder('obs')
+            ->select('obs')
+            ->leftJoin('obs.user', 'user')
+            ->andWhere('user.id = :id')
+            ->setParameter('id', $id)
             ->getQuery()
             ->getScalarResult();
 
-    }
+    }*/
 
     public function getUserValidatedObservations($id)
     {
