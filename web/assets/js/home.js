22235a542543152fb7e218c3aedcbe410ce12eec
Lucien Akchoté <l.akchote@gmail.com>
2017-04-20 08:33:12 +0200

    - Implémentation modale ajout d'une observation dans la route "home" * Modification vue default/index.html.twig + lien fonctionnel vers le flux personnel d'observations (route "observation_flow_user") * Création fichier JS "home.js"
    - Refactoring vue "default/obs.html.twig"
    * Suppression d'une variable et d'un appel à une méthode inutiles

---
+++ web/assets/js/home.js
@@ -0,0 +1,30 @@
+function initMap()
+{
+    var map = document.querySelectorAll('.map');
+    for (var i = 0; i < map.length; i++) {
+        var lng = parseFloat(map[i].getAttribute('data-lng'));
+        var lat = parseFloat(map[i].getAttribute('data-lat'));
+        var latLng = {lat: lat, lng: lng};
+        var loadMap = new google.maps.Map(map[i],
+            {
+                center: latLng,
+                zoom: 15
+            });
+        var setMarker = new google.maps.Marker(
+            {
+                position: latLng,
+                map: loadMap
+            });
+    }
+}
+$('#obs-load-desktop').click(function (e)
+{
+    e.preventDefault();
+    var url = this.getAttribute('data-url');
+    $.ajax({
+        url: url,
+        method: 'GET'
+    }).done(function (msg) {
+        $('#modal-load-desktop').html(msg);
+    });
+});
