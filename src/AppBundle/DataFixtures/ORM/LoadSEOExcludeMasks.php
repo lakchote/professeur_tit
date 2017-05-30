b328ffa72e6077f26088f41ba2d5d3310b41554e
Lucien Akchoté <l.akchote@gmail.com>
2017-05-30 23:01:14 +0200

    - Ajout masques d'exclusion pour fixture LoadSEOExcludeMasks

--- src/AppBundle/DataFixtures/ORM/LoadSEOExcludeMasks.php
+++ src/AppBundle/DataFixtures/ORM/LoadSEOExcludeMasks.php
@@ -9,7 +9,7 @@ use Doctrine\Common\Persistence\ObjectManager;
 
 class LoadSEOExcludeMasks implements FixtureInterface
 {
-    private $excludeMasks = ['connect', 'delete', 'modal', 'login', 'logout', 'register', 'reset'];
+    private $excludeMasks = ['connect', 'delete', 'modal', 'login', 'logout', 'register', 'reset', 'admin', 'obs_', 'PrestaSitemapBundle', 'geoJson', 'show', 'getListing', 'search', 'manage_profil_membre', 'user_public_profile', 'observation_flow_user'];
 
     public function load(ObjectManager $manager)
     {
