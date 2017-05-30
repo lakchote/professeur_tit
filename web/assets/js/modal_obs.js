550deec1f5b93fd10e7606c10732fd519e945874
Lucien Akchoté <l.akchote@gmail.com>
2017-05-30 11:14:57 +0200

    - Ajout gestion des données de la taxref en offline lors de l'ajout d'une observation - Ajout test vérifiant que la ville rentrée en offline existe bien sinon un message d'erreur est affiché à l'utilisateur lorsqu'il retrouve une connexion - Ajout polyfills API fetch et Promises - Refactoring global du code et correction de bugs

--- web/assets/js/modal_obs.js
+++ web/assets/js/modal_obs.js
@@ -1,26 +1,24 @@
-/**
- * Created by Benoit on 09/04/2017.
- */
-
 let isLocalStorage = false;
+let urlCSV = '/assets/csv/taxrefv2.csv';
+let csvOfflineData = [];
 let fileReader = new FileReader();
-let imageObs, recupRechercheOffline = undefined;
+let imageObs = undefined;
 
 function initMap() {
-    var input = (document.getElementById('pac-input'));
-    var options = {
+    let input = (document.getElementById('pac-input'));
+    let options = {
         componentRestrictions: {country: 'fr'}
     };
-    var autocomplete = new google.maps.places.Autocomplete(input, options);
+    let autocomplete = new google.maps.places.Autocomplete(input, options);
 
     autocomplete.addListener('place_changed', function () {
-        var place = autocomplete.getPlace();
+        let place = autocomplete.getPlace();
         if (!place.geometry) {
             window.alert(alert1);
             return;
         }
 
-        var nbAdressComp = place.address_components.length;
+        let nbAdressComp = place.address_components.length;
         for ($i = 0; $i < nbAdressComp; $i++) {
             if (place.address_components[$i].types[0] == 'locality') {
                 var laVille = place.address_components[$i].short_name;
@@ -38,11 +36,11 @@ function initMap() {
 
 }
 function geocodeLatLng(geocoder) {
-    var latlng = {lat: parseFloat($('#obs_form_latitude').val()), lng: parseFloat($('#obs_form_longitude').val())};
+    let latlng = {lat: parseFloat($('#obs_form_latitude').val()), lng: parseFloat($('#obs_form_longitude').val())};
     geocoder.geocode({'location': latlng}, function (results, status) {
         if (status === google.maps.GeocoderStatus.OK) {
             if (results) {
-                var laVille = results[0].address_components[2].short_name;
+                let laVille = results[0].address_components[2].short_name;
                 $('#obs_form_ville').val(laVille);
             } else {
                 window.alert('No results found');
@@ -63,7 +61,7 @@ function showPosition(position) {
     $('#obs_form_longitude').val(position.coords.longitude);
     $('#obs_form_latitude').val(position.coords.latitude);
     $('#pac-input').val(position.coords.latitude + " " + position.coords.longitude);
-    var geocoder = new google.maps.Geocoder;
+    let geocoder = new google.maps.Geocoder;
     geocodeLatLng(geocoder);
 }
 
@@ -73,31 +71,54 @@ function dataURLtoFile(url, name, type) {
         .then(arrayBuffer => new File([arrayBuffer], name, {type: type}));
 }
 
-(function checkLocalStorageData() {
-    let imageStored = localStorage.getItem('obs_form[image]');
-    if (!imageStored)
+(function getCSVDataForOffLine()
+{
+    if(localStorage.getItem('taxref'))
     {
-        localStorage.clear();
+        csvOfflineData = JSON.parse(localStorage.getItem('taxref'));
         return false;
     }
+    fetch(urlCSV)
+        .then(response => response.text())
+        .then(csvData =>
+        {
+            let arrayCsv = csvData.split(/\n/);
+            for(let i = 1; i < arrayCsv.length; ++i)
+            {
+                let filterArrayEntries = arrayCsv[i].split(',');
+                if(filterArrayEntries[10][0] === '"') filterArrayEntries[10] = filterArrayEntries[10].slice(1);
+                csvOfflineData.push({label: filterArrayEntries[10], value: filterArrayEntries[10], nomLatin: filterArrayEntries[7]});
+            }
+            localStorage.setItem('taxref', JSON.stringify(csvOfflineData));
+            csvOfflineData = JSON.parse(localStorage.getItem('taxref'));
+        });
+})();
+
+(function checkLocalStorageData() {
+    let imageStored = localStorage.getItem('obs_form[image]');
+    if (!imageStored) return false;
     dataURLtoFile(imageStored, 'file.jpeg', 'image/jpeg').then(function (file) {
         imageObs = file;
     });
     let ville = localStorage.getItem('localisation');
-    localStorage.setItem('obs_form[ville]', ville);
-    recupRechercheOffline = localStorage.getItem('recherche');
-    let description = localStorage.getItem('obs_form[description]');
-    if (ville && !(localStorage.getItem('obs_form[longitude]')) && navigator.onLine) {
+    if (ville && !(localStorage.getItem('obs_form[longitude]')) && (getOnlineStatus())) {
         fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + ville + '&key=AIzaSyACO55kGEkd8YeNffAaErhE02wa_UigduQ')
             .then(response => response.json())
-            .then(data => {
+            .then(data =>
+            {
+                if((Math.sign(data.results[0].geometry.location.lng.toFixed(10)) === -1) || (Math.sign(data.results[0].geometry.location.lat.toFixed(11)) === -1))
+                {
+                    alert('Ville incorrect.');
+                    return false;
+                }
                 localStorage.setItem('obs_form[longitude]', data.results[0].geometry.location.lng.toFixed(10));
                 localStorage.setItem('obs_form[latitude]', data.results[0].geometry.location.lat.toFixed(11));
             });
     }
+    localStorage.setItem('obs_form[ville]', ville);
     $('#pac-input').val(ville);
-    $('#recherche').val(recupRechercheOffline);
-    $('#obs_form_description').val(description);
+    $('#recherche').val(localStorage.getItem('recherche'));
+    $('#obs_form_description').val(localStorage.getItem('obs_form[description]'));
     fetch(imageStored)
         .then(response => response.blob())
         .then(blob => {
@@ -107,8 +128,13 @@ function dataURLtoFile(url, name, type) {
     isLocalStorage = true;
 })();
 
+function getOnlineStatus()
+{
+    return navigator.onLine;
+}
+
 function checkOnlineStatus(data) {
-    if (!navigator.onLine) {
+    if (!getOnlineStatus()) {
         for (let pair of data.entries()) {
             if (pair[1] instanceof File && pair[1].size > 0) {
                 let key = pair[0];
@@ -117,7 +143,7 @@ function checkOnlineStatus(data) {
                     localStorage.setItem(key, fileReader.result);
                 }
             }
-            localStorage.setItem(pair[0], pair[1]);
+            if(!(pair[0] === 'obs_form[image]')) localStorage.setItem(pair[0], pair[1]);
         }
         alert('Vous ne disposez d\'aucune connexion Internet. Votre observation a été enregistrée');
         return false;
@@ -135,17 +161,26 @@ function getOfflineFormData() {
 }
 
 $('#recherche').autocomplete({
-    source: pathListing,
+    source: getOnlineStatus() ? pathListing : csvOfflineData,
     minLength: 3,
     create: function () {
         $(this).data('ui-autocomplete')._renderItem = function (ul, item) {
-            return $('<li><strong>' + item.value + '</strong> / <i>' + item.label + '</i></li>').appendTo(ul);
+            return (getOnlineStatus()) ? $('<li><strong>' + item.value + '</strong> / <i>' + item.label + '</i></li>').appendTo(ul) : $('<li><strong>' + item.value + '</strong> / <i>' + item.nomLatin + '</i></li>').appendTo(ul);
         };
     },
     select: function (event, ui) {
-        $("#recherche").val(ui.item.value + ' / ' + ui.item.label);
-        $("#obs_form_taxon").val(ui.item.label);
-        return false;
+        if((getOnlineStatus()))
+        {
+            $("#recherche").val(ui.item.value + ' / ' + ui.item.label);
+            $("#obs_form_taxon").val(ui.item.label);
+            return false;
+        }
+        else
+        {
+            $("#recherche").val(ui.item.value + ' / ' + ui.item.nomLatin);
+            $("#obs_form_taxon").val(ui.item.nomLatin);
+            return false;
+        }
     },
 });
 
@@ -155,9 +190,9 @@ $('#location').click(function () {
 });
 
 $('#obs_form_image').on('change', function (e) {
-    var files = $(this)[0].files;
+    let files = $(this)[0].files;
     if (files.length > 0) {
-        var file = files[0];
+        let file = files[0];
         $('#preview').removeClass('hidden');
         $('#theFile').attr('src', window.URL.createObjectURL(file));
     }
@@ -172,11 +207,11 @@ $('#removeImage').on('click', function (e) {
 
 $('#btn-publish').click(function (e) {
     e.preventDefault();
-    var $form = $('form[name="obs_form"]');
-    var recupRechercheOnline = $('#recherche').val();
-    var $formdata = new FormData($form[0]);
+    let $form = $('form[name="obs_form"]');
+    let recupRecherche = $('#recherche').val();
+    let $formdata = new FormData($form[0]);
     if (!checkOnlineStatus($formdata)) return false;
-    var $data = (isLocalStorage === false) ? $formdata : getOfflineFormData();
+    let $data = (isLocalStorage === false) ? $formdata : getOfflineFormData();
     $.ajax
     ({
         url: $form.attr('action'),
@@ -187,7 +222,7 @@ $('#btn-publish').click(function (e) {
         statusCode: {
             201: function (msg) {
                 $('#modal-load-desktop').html(msg);
-                (isLocalStorage === false) ? $('#recherche').val(recupRechercheOnline) : $('#recherche').val(recupRechercheOffline);
+                $('#recherche').val(recupRecherche);
                 $('#pac-input').val($('#obs_form_ville').val());
             },
             200: function () {
